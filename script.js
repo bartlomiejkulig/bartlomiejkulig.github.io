93b02c45aee5244eb02d531a2af79a1fe20db2dd
bartlomiejkulig <bartlomiejkulig2@gmail.com>
2016-11-21 16:05:55 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-var button = document.getElementById("hamburger");
-var header = document.getElementById("page-header");
-button.addEventListener("click", function() {
-  header.classList.toggle("hamburger-opened");
-},false);
